d4b1073a271e6e470233c1992fb266f970686ac3
Raphael <r3campanher@gmail.com>
2023-08-23 19:50:55 -0300

    PrimeiroCommit

---
+++ codigo.js
@@ -0,0 +1,31 @@
+//var nome = "Raphael"; // variável tipo texto
+//var sobrenome = "Campanher"; // texto
+//var anoAtual = 2023; // inteiro
+//var idadeFimdoAno = 15; // inteiro
+//var altura = 1.70; // real
+//var jaComeu = true; // lógico
+
+//console.log(nome); 
+//console.log(sobrenome);
+//console.log(anoAtual);
+//console.log(idadeFimdoAno);
+//console.log(altura);
+//console.log(jaComeu);
+
+//var nomeCompleto = nome + sobrenome
+//var nascimento = idadeFimdoAno - anoAtual
+//console.log(nomeCompleto)
+//console.log(nascimento)
+
+//var insira = prompt( "Indira seu nome")
+// alert
+// console.log
+
+var nome = prompt("Insira seu nome")
+var sobrenome = prompt("Insira seu sobrenome")
+var nomeCompleto = nome +" "+ sobrenome
+alert(nomeCompleto)
+var n1 = prompt("Insira um número para ser somado")
+var n2 = prompt("Insira outro número para ser somado")
+var result = parseInt(n1) + parseInt(n2)
+alert(result)
